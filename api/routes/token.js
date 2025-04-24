14cd2a962e04a76251875b234ad086c8cd66fdfc
Juh <167531264+Juuhj@users.noreply.github.com>
2025-04-24 09:39:54 -0300

    Update token.js

--- api/routes/token.js
+++ api/routes/token.js
@@ -1,4 +1,1 @@
-// {
-//     "message": "Login realizado com sucesso",
-//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibm9tZSI6Ikp1aCIsImVtYWlsIjoianVoY2FycmVpcmFAZ21haWwuY29tIiwiaWF0IjoxNzQ0MzkwOTg2LCJleHAiOjE3NDQzOTQ1ODZ9.SnxxZmAaleZhpsXR092nOz3DMb__iRCyzYMyBwsguos"
-// }
+
